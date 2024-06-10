d57c045294860dfae09562c86300e041ec6298cc
LucasFerruchi <lucasferruchi05@gmail.com>
2024-06-10 17:25:05 -0300

    instalacion de fontawesome

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -82,7 +82,6 @@ const NavBar = () => {
           </div>
         </div>
       </nav>
-      ;
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx";
 //Nuevas dependencias
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
+import "font-awesome/css/font-awesome.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
